a6ecb4534111f5e1047069cc56ef8a3431513db4
Jisub Kim <plumssi9@gmail.com>
2018-04-11 20:23:11 +0900

    create calculator functions create double sum() create double sub() create double mul() create double div()

--- oss01/oss01/main.c
+++ oss01/oss01/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
+double sum(double, double); // ±èÁö¼·
+double sub(double, double); // ÀÌ»óÈñ
+double mul(double, double); // ¾çÀ±¿­
+double div(double, double); // Æ¼¿£¾ó½Ö
+
 int main() {
-	
+	double x, y, result;
 	return 0;
+}
+
+double sum(double a, double b) // ±èÁö¼·
+{
+
+}
+
+double sub(double a, double b) // ÀÌ»óÈñ
+{
+
+}
+
+double mul(double a, double b) // ¾çÀ±¿­
+{
+
+}
+
+double div(double a, double b) // Æ¼¿£¾ó½Ö
+{
+
 }
